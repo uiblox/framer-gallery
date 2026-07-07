dd1e64bd5d624e92c29d7c14696bcf001468d2c4
Christian <bournecreative@gmail.com>
2026-07-06 21:20:59 -0500

    Christen the project. Vite and tailwind starter.

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <section>
+        <h1 className="text-3xl font-bold underline">Lets go</h1>
+      </section>
+    </>
+  );
+}
+
+export default App;
