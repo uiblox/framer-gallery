583b6299cbc4a27fb8046e88443dc81861830f31
Christian <bournecreative@gmail.com>
2026-07-07 22:33:27 -0500

    useFetch: Create useFetch hook to pull in image data via the unsplash api.

---
+++ src/hooks/useFetch.ts
@@ -0,0 +1,27 @@
+import { useEffect, useState } from "react";
+
+type UnsplashImage = {
+  id: string;
+  urls: {
+    regular: string;
+  };
+  alt_description?: string | null;
+};
+
+export const useFetch = () => {
+  const [data, setData] = useState<UnsplashImage[] | null>(null);
+  const ImageAccessKey = import.meta.env.VITE_API_KEY;
+
+  const url = `https://api.unsplash.com/photos/?client_id=${ImageAccessKey}`;
+
+  useEffect(() => {
+    async function fetchImages() {
+      const response = await fetch(url);
+      const imageJSON = (await response.json()) as UnsplashImage[];
+      setData(imageJSON);
+    }
+    fetchImages();
+  }, [url]);
+
+  return { data };
+};
